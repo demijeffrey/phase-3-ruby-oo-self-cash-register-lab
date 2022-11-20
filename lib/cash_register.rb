9f685d2664b3694b4ed9538694f1bc93340b12b4
Demaris Ashmore <demiashmore@yahoo.com>
2022-11-20 12:37:27 -0700

    commit tree 48ad9b567407e5e62f5e1f7aef7f6552f2d732ec

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,39 @@
+require 'pry'
 
+class CashRegister
+
+    attr_reader :discount
+    attr_accessor :total, :items, :last_price
+
+    def initialize(discount = 0)
+        @discount = discount
+        @total = 0
+        @items = []
+        @last_price
+    end
+
+    def add_item(title, price, quantity = 1)
+        quantity.times do
+            self.items << title
+        end
+        @last_price = price * quantity
+        @total += price * quantity
+    end
+
+    def apply_discount
+        if @discount == 0
+            "There is no discount to apply."
+        else
+            new_total = @total * @discount / 100
+            @total -= new_total
+            "After the discount, the total comes to $#{@total}."
+        end
+    end
+
+    def void_last_transaction
+        @total -= @last_price
+    end
+
+    # binding.pry
+
+end
